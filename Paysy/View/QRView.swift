07f2542755996097f77392174fe245b6780741ac
Anıl Demirci <anildemirci1996@gmail.com>
2022-04-27 01:17:13 +0300

    27.04.2022 && 01:17
    Push notification eklendi.

--- Paysy/View/QRView.swift
+++ Paysy/View/QRView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct QRView: View {
     var body: some View {
         VStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("QR CODE")
         }
     }
 }
